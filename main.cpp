441d39ec4bf4e44252e8e614c961fa363394af3b
john <yibo@rongcloud.cn>
2020-09-15 09:05:31 +0800

    feat:edit hello

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <vector>
 
-int main() {
+using namespace std;
+
+int autoTest();
+
+int main(int argc, char **argv) {
     std::cout << "Hello, World ,good to see u!" << std::endl;
+    //autoTest();
     return 0;
+}
+
+
+int autoTest() {
+    vector<int> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    for (auto i : v) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    return 0;
+
 }
